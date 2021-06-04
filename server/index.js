d803b24a30cfd3ea4c57f2298aeb715172720a56
umudik <seyitumutbicer@gmail.com>
2021-06-05 00:20:22 +0300

    dev

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-let Fookie = require('fookie')
+const Fookie = require('fookie')
 const api = new Fookie()
 
-let start = async function(){
-    await api.connect('mongodb://mongo/fookie') 
+let start = async function () {
+    await api.connect('mongodb://mongo/fookie')
     api.listen(8080)
 }
 start()
