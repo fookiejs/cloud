118f18e076ff66687479029f6352a30e5a173091
umudik <seyitumutbicer@gmail.com>
2021-06-12 12:55:08 +0300

    dev

--- landing/src/views/Home.vue
+++ landing/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
     div(class="flex justify-center items-center h-full" style="height:500px;")
         div
+            div hi
             div(class="text-8xl my-6") Fookie JS
             div(class="flex justify-center space-x-8")
                 v-btn(x-large) 
