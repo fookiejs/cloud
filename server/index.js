6fcc1d825ec5cabae4b8ce1c7d70733fa54e7217
umuş <seyitumutbicer@gmail.com>
2021-08-09 01:06:36 +0300

    dev

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const Fookie = require("../../fookie/fookie")
-const fookie = new Fookie()
-
-let start = async function () {
-    await fookie.connect('mongodb://mongo/fookie')
-    fookie.model(require("./src/documentation/model/page.js"))
-    fookie.listen(3000)
-}
-start()
