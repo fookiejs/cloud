68bdb238a824cff5051453c991f2bd60b42a389b
umuş <seyitumutbicer@gmail.com>
2021-10-23 17:39:01 +0300

    dev

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,13 +1,13 @@
-
-
 module.exports.run = async (event) => {
-  const Fookie = require("fookie")
-  let fookie = new Fookie()
-  await fookie.init()
-  await fookie.use(require("./src/mod"))
-  if (typeof event.body.system == "boolean") return false
-  let body = Object.assign({}, JSON.parse(event.body))
-  let res = await fookie.run(body)
+  //  const Fookie = require("fookie");
+  const Fookie = require("../../fookie/src/index");
+  let fookie = new Fookie();
+  await fookie.init();
+  await fookie.use(require("./src/mod"));
+  if (typeof event.body.system == "boolean") return false;
+  let body = Object.assign({}, JSON.parse(event.body));
+  let res = await fookie.run(body);
+  console.log(res);
   return {
     statusCode: 200,
     body: JSON.stringify(res),
